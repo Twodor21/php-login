49400122dc6e64df16c94916350847e91f286ea4
Tudor Popa <Twodor21@users.noreply.github.com>
2021-02-19 18:17:52 +0200

    small change

--- index.php
+++ index.php
@@ -10,9 +10,11 @@
 					}
 			?>			
 		</section>
-
-		
 	
+	<div class="home1">
+		<h1>Hello everyone,</h1><br />
+		<h3>This website is made just to test the Login system<br /> and the Data transfered through the Database.</h3>
+	</div>
 <?php
 	include_once 'php/footer.php';
 ?>
